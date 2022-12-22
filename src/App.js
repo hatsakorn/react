d792284d1b684c1f162ee4a0093e122c43f1dc94
hatsakorn <gornnb@gmail.com>
2022-12-22 15:34:45 +0700

    finish_form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [input,setInput] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="btn btn-danger"><i className='fa-solid fa-check'/></button>
+    // <div>
+    //   <form 
+    //   action='https"//localhost:3000' 
+    //   method='Post' 
+    //   //GET is send input to browser POST send to server
+    //   onSubmit={event => event.preventDefault()}
+    //   // ยกเลิก submit action default ที่ submit form จะส่งไปยัง server
+    //   > 
+    //     <input name='email' value={input} onChange={event => setInput(event.target.value)}/>
+    //     <input name='password'/>
+    //     <input type = 'file' onChange={event => console.log(event)}/>
+    //     <button>Submit form</button>
+    //   {/* type = 'submit' will send data to server but other type eg. type = 'reset' will not send data to server */}
+    //   </form>
+    //     <button>Submit form outside</button>
+    //     {/* submit outside form จะไม่ส่งค่าไปยัง server */}
+    // </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
